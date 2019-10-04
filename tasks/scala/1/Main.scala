435b32bcf31f606401680f24aa77d9d0fe49b19d
andreisoprachev <soprachev@mail.ru>
2019-10-04 11:20:44 +0300

    Pascal's Triangle

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -14,6 +14,16 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+    if (c < 0 || r < 0 || c > r) {
+      throw new IllegalArgumentException("Invalid arguments")
+    }
+
+    if (c == 0 || c == r) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+
 
   }
 
@@ -21,7 +31,7 @@ object Main {
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    false
   }
 
   /**
@@ -32,6 +42,6 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    0
   }
 }
